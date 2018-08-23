65a5ebe4f7823e67a3b0a9a24fef9ec0927b3dc5
ravi5895 <ravikiran5895@gmail.com>
2018-08-23 20:49:49 +0530

    Dialog2

--- src/app/manage/manage.component.ts
+++ src/app/manage/manage.component.ts
@@ -9,13 +9,13 @@ import {MatDialog} from '@angular/material';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-  reactiveForm:FormGroup;
-  firstname = "Test";
+  reactiveForm: FormGroup;
+  firstname = 'Test';
 
   constructor(public dialog: MatDialog) {
     this.reactiveForm = new FormGroup({
-      'name':new FormControl('', [Validators.required]),
-      'age':new FormControl('',[Validators.required])
+      'name': new FormControl('', [Validators.required]),
+      'age': new FormControl('',[Validators.required])
     });
    }
 
@@ -24,15 +24,15 @@ export class ManageComponent implements OnInit {
 
   submitMyForm(formData) {
     console.log(formData.value);
-    //alert(formData.value.firstname);
+    // alert(formData.value.firstname);
   }
   openDialog(): void {
     const dialogRef = this.dialog.open(RegisterComponent, {
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The dialog was closed');
+    // });
   }
 
 }
